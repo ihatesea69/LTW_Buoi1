1004d00be83245bc61c82bd7e6f19200e2b4e172
ihatesea <ihatesea69@gmail.com>
2024-09-09 11:10:49 +0700

    init

---
+++ Bai01.js
@@ -0,0 +1,9 @@
+﻿const OnClickHandle = () => {
+    const linkss = "https://tuyensinh.huflit.edu.vn/tuyen-sinh";
+    window.open(linkss, "_blank");
+}
+
+const OnClickHandle2 = () => {
+    const linkss = "https://huflit.edu.vn/gioi-thieu-chung/chung-toi-la-ai/";
+    window.open(linkss, "_blank");
+}
